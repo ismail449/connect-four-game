e5f15821c7b6b97be7201c9e54e528b27fd96305
ismail449 <esmael449@gmail.com>
2026-08-28 19:22:53 +0300

    chore: update rules-button component to use Link instead of button html element.

--- app/components/rules-button/rules-button.tsx
+++ app/components/rules-button/rules-button.tsx
@@ -1,21 +1,11 @@
-import { Link } from "react-router";
+import { Link, type Path } from "react-router";
 
 import styles from "./rules-button.module.css";
 
 type Props = {
-  to:
-    | string
-    | {
-        pathname: string;
-        search: string;
-        hash: string;
-      };
+  to: string | Path;
 };
 
 export const RulesButton = ({ to }: Props) => {
-  return (
-    <Link to={to}>
-      <button className={styles.rulesButton} />
-    </Link>
-  );
+  return <Link to={to} className={styles.rulesButton} />;
 };
