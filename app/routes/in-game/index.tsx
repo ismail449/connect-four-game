bdbbb8925ae53369eb2f21ffd7ceff9b7da5a0f3
ismail449 <esmael449@gmail.com>
2026-09-03 21:48:20 +0300

    feat: add the InGameNavBar component to the InGame page.

--- app/routes/in-game/index.tsx
+++ app/routes/in-game/index.tsx
@@ -1,8 +1,16 @@
+import InGameNaveBar from "~/components/in-game-nav-bar/in-game-nav-bar";
+
 import styles from "./index.module.css";
 export function meta() {
   return [{ title: "In Game" }];
 }
 
 export default function InGame() {
-  return <div className={styles.inGamePage}></div>;
+  return (
+    <div className={styles.inGamePage}>
+      <div className={styles.gameContainer}>
+        <InGameNaveBar />
+      </div>
+    </div>
+  );
 }
